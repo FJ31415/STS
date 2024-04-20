470f71d9d5671089f9b8bd710818064547729def
FJ_ <109368000+FJ31415@users.noreply.github.com>
2024-04-20 16:32:29 +0200

    add GameMap

---
+++ src/gamelogic/GameMap.java
@@ -0,0 +1,18 @@
+package gamelogic;
+
+public final class GameMap {
+
+    // TODO impl
+
+    public GameEntity getEntityAt(int x, int y) {
+    }
+
+    public boolean isOutOfBounds(int x, int y) {
+    }
+
+    public boolean isOutOfBounds(Position position) {
+    }
+
+    public GameEntity getEntityAt(Position position) {
+    }
+}
