b974fe7e598d5c096bb4b553f4c99226d60119dc
FJ_ <109368000+FJ31415@users.noreply.github.com>
2024-05-22 10:41:28 +0200

    Added Capital

---
+++ src/gamelogic/buildings/Capital.java
@@ -0,0 +1,19 @@
+package gamelogic.buildings;
+
+import gamelogic.Position;
+import gamelogic.entity.EntityGraphic;
+import gamelogic.nations.Team;
+import ui.graphics.Animation;
+import ui.graphics.Sprite;
+
+public final class Capital extends Building {
+    public Capital(Team team, Position position) {
+        super("Capital", team, 4, 0, 0, 0, 0,
+                3, position, new EntityGraphic(team, new Animation[]{
+                        new Animation(Sprite.CAPITAL_ONE_HEALTH_1, Sprite.CAPITAL_ONE_HEALTH_2),
+                        new Animation(Sprite.CAPITAL_TWO_HEALTH_1, Sprite.CAPITAL_TWO_HEALTH_2),
+                        new Animation(Sprite.CAPITAL_THREE_HEALTH_1, Sprite.CAPITAL_THREE_HEALTH_2),
+                        new Animation(Sprite.CAPITAL_FOUR_HEALTH_1, Sprite.CAPITAL_FOUR_HEALTH_2),
+                }));
+    }
+}
